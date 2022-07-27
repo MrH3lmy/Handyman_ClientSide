c6722fdc87d51b86039c5f5be73860e434b80003
Ahmed Helmy <63620526+MrH3lmy@users.noreply.github.com>
2022-07-27 18:55:40 +0200

    Request Changed

--- src/app/auth/request/request.component.ts
+++ src/app/auth/request/request.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { RequestsService } from 'src/app/services/RequestS.service.ts';
+import { RegisterService } from 'src/app/services/register.service';
+// import { RequestsService } from 'src/app/services/RequestS.service';
+import { RequestService } from 'src/app/services/Request.service';
+
 
 @Component({
   selector: 'app-request',
@@ -10,7 +13,7 @@ import { RequestsService } from 'src/app/services/RequestS.service.ts';
 export class RequestComponent implements OnInit {
   formGroup: FormGroup;
 
-  constructor() {}
+  constructor(private registerService:RegisterService) {}
 
   ngOnInit(): void {}
 }
